86bf063efe94ff2cb44471574f1a14bea7bac9b1
nhuandt26 <dothenhuan26@gmail.com>
2024-03-09 10:21:59 +0700

    Sign in on one device

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,14 +4,52 @@ namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
+
+    protected function authenticate($request, array $guards)
+    {
+
+        if (empty($guards)) {
+            $guards = [null];
+        }
+
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                $checkDevice = $this->checkDevice($request);
+                if (!$checkDevice) {
+//                    return redirect(route("login"));
+                    $this->unauthenticated($request, $guards);
+                }
+                return $this->auth->shouldUse($guard);
+            }
+        }
+
+        $this->unauthenticated($request, $guards);
+    }
+
+
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
     }
+
+    private function checkDevice(Request $request)
+    {
+        $sessionId = $request->session()->getId();
+        $user = $request->user();
+        $lastSessionId = $user->last_session;
+        if ($lastSessionId !== $sessionId) {
+            Auth::logout();
+            return false;
+        }
+        return true;
+    }
+
+
 }
