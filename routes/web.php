2212b70450ba0e2469d7ec4b60e844f9a68edb49
nhuandt26 <dothenhuan26@gmail.com>
2024-03-09 14:32:37 +0700

    Login With mail OTP

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use Ichtrojan\Otp\Otp;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\OtpNotification;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +24,21 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get("/test-otp", function () {
+////    $otp = (new Otp)->generate('admin@gmail.com', 'numeric', 4, 15);
+//    $otp = (new Otp)->validate('admin@gmail.com', "8359");
+//    dd($otp);
+
+//    dd(generateOtp("admin@gmail.com"));
+//    dd(validateOtp("admin@gmail.com", "375013"));
+
+    $otp = generateOtp("admin@gmail.com");
+
+    Notification::route("mail", "dothenhuan26@gmail.com")->notify(new OtpNotification($otp));
+
+});
+
+Route::get("/2fa", [LoginController::class, "get2faForm"])->name("2fa");
+Route::post("/2fa", [LoginController::class, "handle2fa"]);
+
